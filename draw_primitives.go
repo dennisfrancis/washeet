6fecf27f582228294ea4e2d6a702a2c9a71672e2
Dennis Francis <dennis.francis@collabora.co.uk>
2018-10-13 15:36:31 +0530

    implemented line drawing primitives

--- draw_primitives.go
+++ draw_primitives.go
@@ -4,20 +4,37 @@ import (
 	"syscall/js"
 )
 
+var path2dCtor = js.Global().Get("Path2D")
+
 func drawVertLines(canvasContext *js.Value, xcoords []float64, ylow, yhigh float64, color string) {
+	path := path2dCtor.New()
+	canvasContext.Set("strokeStyle", color)
 	for _, xcoord := range xcoords {
-		drawLine(canvasContext, xcoord, ylow, xcoord, yhigh, color)
+		addLine2Path(&path, xcoord, ylow, xcoord, yhigh)
 	}
+	canvasContext.Call("stroke", path)
 }
 
 func drawHorizLines(canvasContext *js.Value, ycoords []float64, xlow, xhigh float64, color string) {
+	path := path2dCtor.New()
+	canvasContext.Set("strokeStyle", color)
 	for _, ycoord := range ycoords {
-		drawLine(canvasContext, xlow, ycoord, xhigh, ycoord, color)
+		addLine2Path(&path, xlow, ycoord, xhigh, ycoord)
 	}
+	canvasContext.Call("stroke", path)
 }
 
 func drawLine(canvasContext *js.Value, xlow, ylow, xhigh, yhigh float64, color string) {
+	path := path2dCtor.New()
+	canvasContext.Set("strokeStyle", color)
+	addLine2Path(&path, xlow, ylow, xhigh, yhigh)
+	canvasContext.Call("stroke", path)
+}
 
+// Adds a line to the path, no actual drawing takes place
+func addLine2Path(path *js.Value, xlow, ylow, xhigh, yhigh float64) {
+	path.Call("moveTo", xlow, ylow)
+	path.Call("lineTo", xhigh, yhigh)
 }
 
 func noStrokeFillRect(canvasContext *js.Value, xlow, ylow, xhigh, yhigh float64, fillColor string) {
