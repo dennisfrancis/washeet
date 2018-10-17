c8f0f3949761652206e8803d8aee3d0476ef94be
Dennis Francis <dennisfrancis.in@gmail.com>
2018-10-17 23:46:52 +0530

    implement col/row label generators

--- locals.go
+++ locals.go
@@ -1,5 +1,13 @@
 package washeet
 
+import (
+	"fmt"
+)
+
+const (
+	letters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
 func minInt64(n1 int64, n2 int64) int64 {
 	result := n1
 	if n2 < n1 {
@@ -17,9 +25,14 @@ func maxInt64(n1 int64, n2 int64) int64 {
 }
 
 func col2ColLabel(nCol int64) string {
+	label := ""
+	for {
+		label = label + letters[nCol%26]
+		nCol /= 26
+	}
 	return "COL"
 }
 
 func row2RowLabel(nRow int64) string {
-	return "ROW"
+	return fmt.Sprintf("%d", nRow+1)
 }
