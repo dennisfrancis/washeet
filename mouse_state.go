44e209c9d46bda43cb5cd16698b83c82b64e7ead
Dennis Francis <dennis.francis@collabora.co.uk>
2018-10-22 00:17:56 +0530

    implement mouse-state getters

--- mouse_state.go
+++ mouse_state.go
@@ -1,5 +1,13 @@
 package washeet
 
+func (self *MouseState) isLeftDown() bool {
+	return 0x01 == (*self & 0x01)
+}
+
+func (self *MouseState) isRightDown() bool {
+	return 0x02 == (*self & 0x02)
+}
+
 func (self *MouseState) setLeftDown() {
 	*self |= 0x01
 }
