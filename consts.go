ca1b8bd6c23adbba4b2045d3467521812ec590c1
Dennis Francis <dennis.francis@collabora.co.uk>
2018-11-05 14:13:35 +0530

    adjust consts to beautify things a bit

--- consts.go
+++ consts.go
@@ -5,15 +5,15 @@ const (
 	MAXROW                   int64   = MAXROWCOUNT - 1
 	MAXCOLCOUNT              int64   = 16384
 	MAXCOL                   int64   = MAXCOLCOUNT - 1
-	DEFAULT_CELL_WIDTH       float64 = 120.0
-	DEFAULT_CELL_HEIGHT      float64 = 30.0
+	DEFAULT_CELL_WIDTH       float64 = 90.0
+	DEFAULT_CELL_HEIGHT      float64 = 25.0
 	SHEET_PAINT_QUEUE_LENGTH int     = 10
 
-	GRID_LINE_COLOR           string = "rgba(50, 50, 50, 1.0)"
+	GRID_LINE_COLOR           string = "rgba(200, 200, 200, 1.0)"
 	CELL_DEFAULT_FILL_COLOR   string = "rgba(255, 255, 255, 1.0)"
 	CELL_DEFAULT_STROKE_COLOR string = "rgba(0, 0, 0, 1.0)" // fonts etc.
 	CURSOR_STROKE_COLOR       string = "rgba(0, 0, 0, 1.0)"
-	HEADER_FILL_COLOR         string = "rgba(200, 200, 200, 1.0)"
+	HEADER_FILL_COLOR         string = "rgba(240, 240, 240, 1.0)"
 	SELECTION_STROKE_COLOR    string = "rgba(0, 0, 50, 0.9)"
 	SELECTION_FILL_COLOR      string = "rgba(0, 0, 200, 0.2)"
 )
