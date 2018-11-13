84b5f205a2ae0a925980b384cbcf7ca233fa64ca
Dennis Francis <dennis.francis@collabora.co.uk>
2018-11-13 18:40:22 +0530

    self -> mouse in mouse_state.go

--- mouse_state.go
+++ mouse_state.go
@@ -1,27 +1,27 @@
 package washeet
 
-func (self *MouseState) isLeftDown() bool {
-	return 0x01 == (*self & 0x01)
+func (mouse *MouseState) isLeftDown() bool {
+	return 0x01 == (*mouse & 0x01)
 }
 
-func (self *MouseState) isRightDown() bool {
-	return 0x02 == (*self & 0x02)
+func (mouse *MouseState) isRightDown() bool {
+	return 0x02 == (*mouse & 0x02)
 }
 
-func (self *MouseState) setLeftDown() {
-	(*self) |= 0x01
+func (mouse *MouseState) setLeftDown() {
+	(*mouse) |= 0x01
 }
 
-func (self *MouseState) setLeftUp() {
-	(*self) &= 0xFE
+func (mouse *MouseState) setLeftUp() {
+	(*mouse) &= 0xFE
 }
 
-func (self *MouseState) setRightDown() {
-	(*self) |= 0x02
+func (mouse *MouseState) setRightDown() {
+	(*mouse) |= 0x02
 }
 
-func (self *MouseState) setRightUp() {
-	(*self) &= 0xFD
+func (mouse *MouseState) setRightUp() {
+	(*mouse) &= 0xFD
 }
 
 func defaultMouseState() MouseState {
