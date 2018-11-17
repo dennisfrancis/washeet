8314b5a998b07634d6b48927652f01dc0b516ccb
Dennis Francis <dennis.francis@collabora.com>
2018-11-17 16:58:45 +0530

    create a global defaultColors settings

---
+++ colors.go
@@ -0,0 +1,13 @@
+package washeet
+
+var (
+	defaultColors = colorSettings{
+		gridLine:        "rgba(200, 200, 200, 1.0)",
+		cellFill:        "rgba(255, 255, 255, 1.0)",
+		cellStroke:      "rgba(0, 0, 0, 1.0)",
+		cursorStroke:    "rgba(0, 0, 0, 1.0)",
+		headerFill:      "rgba(240, 240, 240, 1.0)",
+		selectionStroke: "rgba(100, 156, 244, 1.0)",
+		selectionFill:   "rgba(100, 156, 244, 0.2)",
+	}
+)
