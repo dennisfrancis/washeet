804766ebb4ec8f639bf95df5533a237f49a59deb
Dennis Francis <dennis.francis@collabora.com>
2018-11-27 10:50:27 +0530

    add test case for newRGBAColor constructed Color

--- colors_test.go
+++ colors_test.go
@@ -8,7 +8,18 @@ import (
 func TestColorRGB(t *testing.T) {
 	red, green, blue, alpha := uint8(232), uint8(34), uint8(56), uint8(255)
 	color := NewColor(red, green, blue)
+	alphaf := float32(alpha) / float32(255)
+	colorTester(red, green, blue, alphaf, color, t)
+}
 
+func TestColorRGBA(t *testing.T) {
+	red, green, blue, alpha := uint8(232), uint8(34), uint8(56), uint8(51)
+	color := newRGBAColor(red, green, blue, alpha)
+	alphaf := float32(alpha) / float32(255)
+	colorTester(red, green, blue, alphaf, color, t)
+}
+
+func colorTester(red, green, blue uint8, alphaf float32, color *Color, t *testing.T) {
 	if color.Red() != red {
 		t.Error("Red() : Expected", red, "got", color.Red())
 	}
@@ -21,7 +32,6 @@ func TestColorRGB(t *testing.T) {
 		t.Error("Blue() : Expected", blue, "got", color.Blue())
 	}
 
-	alphaf := float32(alpha) / float32(255)
 	if color.alpha() != alphaf {
 		t.Error("alpha() : Expected", alphaf, "got", color.alpha())
 	}
