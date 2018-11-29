1a4083e4bf077f705eaee819fdfd8d5d1799d3d0
Dennis Francis <dennis.francis@collabora.com>
2018-11-29 09:57:14 +0530

    added setters for textAttribs type

--- textattribs.go
+++ textattribs.go
@@ -1,10 +1,10 @@
 package washeet
 
-var (
-	defaultTextAttributes textAttribs
-	boldAttrib            textAttribs = 0x01
-	italicsAttrib         textAttribs = 0x02
-	underlineAttrib       textAttribs = 0x04
+const (
+	defaultTextAttributes textAttribs = iota
+	boldAttrib            textAttribs = 1 << (iota - 1) // 1
+	italicsAttrib                                       // 2
+	underlineAttrib                                     // 4
 )
 
 func newDefaultTextAttributes() *textAttribs {
@@ -12,14 +12,34 @@ func newDefaultTextAttributes() *textAttribs {
 	return &txtAttribs
 }
 
-func (tattribs *textAttribs) IsBold() bool {
+func (tattribs *textAttribs) isBold() bool {
 	return (*tattribs & boldAttrib) == boldAttrib
 }
 
-func (tattribs *textAttribs) IsItalics() bool {
+func (tattribs *textAttribs) isItalics() bool {
 	return (*tattribs & italicsAttrib) == italicsAttrib
 }
 
-func (tattribs *textAttribs) IsUnderline() bool {
-	return (*tattribs & underlineAttrib) != underlineAttrib
+func (tattribs *textAttribs) isUnderline() bool {
+	return (*tattribs & underlineAttrib) == underlineAttrib
+}
+
+func (tattribs *textAttribs) setField(flag bool, field textAttribs) {
+	if flag {
+		*tattribs |= field
+	} else {
+		*tattribs &^= field
+	}
+}
+
+func (tattribs *textAttribs) setBold(flag bool) {
+	tattribs.setField(flag, boldAttrib)
+}
+
+func (tattribs *textAttribs) setItalics(flag bool) {
+	tattribs.setField(flag, italicsAttrib)
+}
+
+func (tattribs *textAttribs) setUnderline(flag bool) {
+	tattribs.setField(flag, underlineAttrib)
 }
