a01b75fe51ab823d1510bc07b37bb0447d2acc02
Dennis Francis <dennis.francis@collabora.com>
2018-12-13 11:21:20 +0530

    major rewrite of demo that uses SheetNotifier iface

--- demo/main.go
+++ demo/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"syscall/js"
+	"time"
 
 	"github.com/dennisfrancis/washeet"
 )
 
-type SheetModel struct {
-}
-
 var (
+	blackColor *washeet.Color = washeet.NewColor(0, 0, 0)
+	whiteColor *washeet.Color = washeet.NewColor(255, 255, 255)
 	redColor   *washeet.Color = washeet.NewColor(200, 0, 0)
 	greenColor *washeet.Color = washeet.NewColor(0, 200, 0)
 	blueColor  *washeet.Color = washeet.NewColor(0, 0, 200)
@@ -21,38 +22,135 @@ var (
 	lblueColor  *washeet.Color = washeet.NewColor(230, 255, 255)
 )
 
+const (
+	numLocations int64 = 20
+	numMeasures  int64 = 10
+)
+
+func maxI64(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func minI64(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+type DataModel struct {
+	measures     []float64
+	locnames     []string
+	measurenames []string
+	enabled      bool
+	sModel       *SheetModel
+}
+
+func newDataModel(sModel *SheetModel) *DataModel {
+	dm := &DataModel{enabled: true, sModel: sModel}
+	dm.measures = make([]float64, numLocations*numMeasures)
+	dm.measurenames = make([]string, numMeasures)
+	dm.locnames = make([]string, numLocations)
+	for locidx := int64(0); locidx < numLocations; locidx++ {
+		dm.locnames[locidx] = fmt.Sprintf("LC#%d", locidx)
+	}
+
+	for midx := int64(0); midx < numMeasures; midx++ {
+		dm.measurenames[midx] = fmt.Sprintf("MSR#%d", midx)
+	}
+
+	return dm
+}
+
+func (dmodel *DataModel) getMeasure(lidx, midx int64) float64 {
+	if lidx < int64(0) || lidx >= numLocations || midx < int64(0) || midx >= numMeasures {
+		return 0.0
+	}
+	return dmodel.measures[lidx*numMeasures+midx]
+}
+
+func (dmodel *DataModel) getMeasureName(midx int64) string {
+	if midx < int64(0) || midx >= numMeasures {
+		return ""
+	}
+	return dmodel.measurenames[midx]
+}
+
+func (dmodel *DataModel) getLocationName(lidx int64) string {
+	if lidx < int64(0) || lidx >= numLocations {
+		return ""
+	}
+	return dmodel.locnames[lidx]
+}
+
+func (dmodel *DataModel) runModel() {
+	for dmodel.enabled {
+		midx := rand.Int63n(numMeasures)
+		lidx := rand.Int63n(numLocations)
+		curr := &(dmodel.measures[lidx*numMeasures+midx])
+		delta := (rand.Float64() - 0.5) / 10.0
+		*curr += delta
+		dmodel.sModel.notify(lidx, midx)
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+type SheetModel struct {
+	sheetNotifier washeet.SheetNotifier
+	dmodel        *DataModel
+	startCol      int64
+	startRow      int64
+}
+
+func (self *SheetModel) notify(locationIdx, measureIdx int64) {
+	if self.sheetNotifier == nil {
+		return
+	}
+	self.sheetNotifier.UpdateCell(self.startCol+1+measureIdx, self.startRow+1+locationIdx)
+}
+
 // Satisfy SheetDataProvider interface.
 
 func (self *SheetModel) GetDisplayString(column int64, row int64) string {
-	if column == 5 && row == 5 {
-		return "Hello washeet !"
+	if column < self.startCol || column > (self.startCol+numMeasures) ||
+		row < self.startRow || row > (self.startRow+numLocations) {
+		return ""
 	}
 
-	return fmt.Sprintf("Cell(%d, %d)", column, row)
+	if column == self.startCol && row == self.startRow {
+		return "Measures/Locations"
+	}
+
+	if column == self.startCol {
+		return self.dmodel.getLocationName(row - self.startRow - 1)
+	}
+
+	if row == self.startRow {
+		return self.dmodel.getMeasureName(column - self.startCol - 1)
+	}
+
+	return fmt.Sprintf("%.3f", self.dmodel.getMeasure(row-self.startRow-1, column-self.startCol-1))
 }
 
 func (self *SheetModel) GetCellAttribs(column, row int64) *washeet.CellAttribs {
-	idx := column % int64(4)
 	attrib := washeet.NewDefaultCellAttribs()
-	if idx == 1 {
-		attrib.SetBold(true)
-	} else if idx == 2 {
-		attrib.SetItalics(true)
-	} else if idx == 3 {
-		attrib.SetUnderline(true)
+
+	if column < self.startCol || column > (self.startCol+numMeasures) ||
+		row < self.startRow || row > (self.startRow+numLocations) {
+		return attrib
 	}
 
-	idxrow := row % int64(9)
-	if idxrow < 3 {
-		attrib.SetAlignment(washeet.AlignLeft)
-		attrib.SetFGColor(redColor)
-		if idxrow == 2 {
-			attrib.SetFontSize(9)
-		}
-	} else if idxrow < 6 {
+	if column == self.startCol || row == self.startRow {
+		attrib.SetBold(true)
+		attrib.SetUnderline(true)
 		attrib.SetAlignment(washeet.AlignCenter)
-		attrib.SetFGColor(greenColor)
-		attrib.SetBGColor(lgreenColor)
+		attrib.SetFGColor(whiteColor)
+		attrib.SetBGColor(blackColor)
 	} else {
 		attrib.SetAlignment(washeet.AlignRight)
 		attrib.SetFGColor(blueColor)
@@ -63,20 +161,42 @@ func (self *SheetModel) GetCellAttribs(column, row int64) *washeet.CellAttribs {
 }
 
 func (self *SheetModel) GetColumnWidth(column int64) float64 {
-	if column == 5 {
-		return 230.0
+	if column == self.startCol {
+		return 150.0
 	}
 	return 0.0
 }
 
 func (self *SheetModel) GetRowHeight(row int64) float64 {
-	if row == 5 {
-		return 70.0
-	}
 	return 0.0
 }
 
 func (self *SheetModel) TrimToNonEmptyRange(c1, r1, c2, r2 *int64) bool {
+
+	endCol := self.startCol + numMeasures
+	endRow := self.startRow + numLocations
+
+	if c1 == nil || c2 == nil || r1 == nil || r2 == nil {
+		return false
+	}
+
+	if *c2 < *c1 || *r2 < *r1 {
+		return false
+	}
+
+	if *c2 < self.startCol || *c1 > endCol {
+		return false
+	}
+
+	if *r2 < self.startRow || *r1 > endRow {
+		return false
+	}
+
+	*c1 = maxI64(*c1, self.startCol)
+	*c2 = minI64(*c2, endCol)
+	*r1 = maxI64(*r1, self.startRow)
+	*r2 = minI64(*r2, endRow)
+
 	return true
 }
 
@@ -97,11 +217,17 @@ func main() {
 	})
 	closeButton.Call("addEventListener", "click", closeHandler)
 
-	model := &SheetModel{}
+	model := &SheetModel{startCol: 1, startRow: 1}
+	dataModel := newDataModel(model)
+	model.dmodel = dataModel
 	sheet := washeet.NewSheet(&container, width, height, model)
+	model.sheetNotifier = sheet
+	go dataModel.runModel()
 	sheet.Start()
 
 	<-quit
+	dataModel.enabled = false
+	time.Sleep(250 * time.Millisecond)
 	closeButton.Call("removeEventListener", "click", closeHandler)
 	closeHandler.Release()
 	sheet.Stop()
